90a8ce7c3e1c1ace06135e58ea077ae6e3dcd176
John Rodriguez <jarodriguez53@my.scccd.edu>
2024-01-19 11:46:32 -0800

    Added the second code along

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+# Code along 1
 UserInput = input("Enter your name: ")
 print("Hello", UserInput)
+
+# Code along 2
+hours_worked = input("Enter Hours: ")
+pay_rate = input("Rate: ")
+total_pay = float(hours_worked) * float(pay_rate)
+print("Pay: ", total_pay)
