a0412e691375c3138aec8eba13702f1603dc9749
John Rodriguez <jarodriguez53@my.scccd.edu>
2024-01-25 17:59:45 -0800

    created new file for this weeks code along

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ hours_worked = input("Enter Hours: ")
 pay_rate = input("Rate: ")
 float_hours = float(hours_worked)
 float_rate = float(pay_rate)
-print(float_hours, float_rate)
+#print(float_hours, float_rate)
 if float_hours > 40:
-    print("Overtime")
+    #print("Overtime")
     reg = float_rate * float_hours
     otp = (float_hours - 40.0) * (float_rate * 0.5)
-    print(reg,otp)
+    #print(reg,otp)
     total_pay = reg + otp
 else:
-    print("Regular")
+    #print("Regular")
     total_pay = float(hours_worked) * float(pay_rate)
 print("Pay: ", total_pay)
