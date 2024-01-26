2b47d0e937f57930dce0a6531f80c68467898c5f
John Rodriguez <jarodriguez53@my.scccd.edu>
2024-01-25 17:50:48 -0800

    Updated 1/25/24 week 2 code along 1

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-# Code along 1
+
 UserInput = input("Enter your name: ")
 print("Hello", UserInput)
 
-# Code along 2
 hours_worked = input("Enter Hours: ")
 pay_rate = input("Rate: ")
-total_pay = float(hours_worked) * float(pay_rate)
+float_hours = float(hours_worked)
+float_rate = float(pay_rate)
+print(float_hours, float_rate)
+if float_hours > 40:
+    print("Overtime")
+    reg = float_rate * float_hours
+    otp = (float_hours - 40.0) * (float_rate * 0.5)
+    print(reg,otp)
+    total_pay = reg + otp
+else:
+    print("Regular")
+    total_pay = float(hours_worked) * float(pay_rate)
 print("Pay: ", total_pay)
